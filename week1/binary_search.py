542c408ff5bb03a4df5ea27e209f48a53f9bdf05
harshitha090 <gharshitha090@gmail.com>
2026-06-21 13:49:32 +0530

    Add Binary Search algorithm

---
+++ week1/binary_search.py
@@ -0,0 +1,20 @@
+def binary_search(arr, target):
+    left = 0
+    right = len(arr) - 1
+
+    while left <= right:
+        mid = (left + right) // 2
+
+        if arr[mid] == target:
+            return mid
+        elif arr[mid] < target:
+            left = mid + 1
+        else:
+            right = mid - 1
+
+    return -1
+
+arr = [22, 33, 44, 55, 66, 77, 88, 99]
+target = 66
+
+print(binary_search(arr, target))
